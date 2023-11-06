fa9199d00c8f2c6bee6cb6d9c07e2320663dd8b9
eeunn <eeun45@gmail.com>
2023-11-06 11:23:33 +0900

    project commit

---
+++ Source/NCA_1/NCA_1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NCA_1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NCA_1, "NCA_1" );
